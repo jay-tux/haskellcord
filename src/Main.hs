84ead0a4b9c924073089e639e79895b205f9cea9
jay-tux <sys.jonas@yahoo.com>
2020-11-18 16:13:37 +0100

    Basic bot runs

---
+++ src/Main.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+
+import Control.Monad (when)
+import Data.Text (isPrefixOf, toLower, Text)
+import qualified Data.Text.IO as TIO
+
+import UnliftIO
+import UnliftIO.Concurrent
+
+import Discord
+import Discord.Types
+import qualified Discord.Requests as R
+
+main :: IO ()
+main = pingpongExample
+
+-- | Replies "pong" to every message that starts with "ping"
+pingpongExample :: IO ()
+pingpongExample = do userFacingError <- runDiscord $ def
+                                            { discordToken = "Nzc4NTc4NzE0OTM3MTMxMDI5.X7UB7g.zpv0tBFnIHq7BRyxPRGGvkrfg_s"
+                                            , discordOnEvent = eventHandler }
+                     TIO.putStrLn userFacingError
+
+eventHandler :: Event -> DiscordHandler ()
+eventHandler event = case event of
+       MessageCreate m -> when (not (fromBot m) && isPing (messageText m)) $ do
+--               _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
+--               threadDelay (4 * 10^6)
+               _ <- restCall (R.CreateMessage (messageChannel m) "Pong!")
+               pure ()
+       _ -> pure ()
+
+fromBot :: Message -> Bool
+fromBot m = userIsBot (messageAuthor m)
+
+isPing :: Text -> Bool
+isPing = ("ping" `isPrefixOf`) . toLower
